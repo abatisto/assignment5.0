639873dafb456c01ca968fa23bb13562dc30f700
amreissfelder <amreissfelder@gmail.com>
2016-05-09 15:09:34 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -3,26 +3,47 @@
     include ("header.php");
     include ("nav.php");
 ?>
-        
-    <h2 id='home_head'>Welcome!</h2>
+               
+    <article id="main">
+    <h2 id='news_h2'>News</h2>
+    <h3>> Cold Era to Play at Coachella 2017</h3>
+    <p>
+        <a href="pics/coachella_poster.jpg" target="_blank">
+        <img class='h3img' src='pics/coachella_poster.jpg' width="30%" height="30%" alt='Coachella lineup for 2017'>
+        </a>
+        The Coachella 2017 lineup has finally been released and Cold Era will be headlining 
+        Day 2!  Buy your tickets <a href='https://www.coachella.com/festival-passes/'>here</a> for 
+        Saturday, April 14th to catch Cold Era's show as well as performances by Neutral Milk Hotel,
+        Modest Mouse, System of a Down, Silversun Pickups, and more! 
+    </p>
     
-    <figure>
+    <h3>> Added Tour Dates</h3>
+    <p>
+        Cold Era has released two new tour dates!  You can now catch them live at The Sherman Theater, East 
+        Stroudsburg PA on July 8<sup>th</sup> and at The Space, Hamden CT on July 13<sup>th</sup>.  Tickets are
+        selling out fast so buy yours today!
+    </p>
     
-    <img id='image' src="austindrums.jpg" alt="Coffee House 2012"/>
-    <figcaption>Featured artist: Austin Batistoni, MHS Coffee House 2012.</figcaption>
-    </figure>
+    <h3>> Tentative Release Date for New Album</h3>
+    <p>
+        <img class='h3img' src='pics/coldera.jpg' width="30%" height="30%" alt='Cold Era album cover'>
+        Finally the wait is over!  Cold Era has announced the release date for their new album to be some time
+        in July this summer.  A few songs off the album have been revealed, such as <i>Sandbox, Shit,</i> and <i>Wires
+            for Breakfast.</i>  Sign up for the <a href="form.php">mailing list</a> so you can pre-order the album 
+            when it's available.  
+    </p>
     
- 
-    <p id='home'>Welcome to Drum Lover Magazine! Here, we love all things related to 
-    drums and percussion! A subscription to Drum Lover Magazine gets you a full year 
-    of monthly issues to fulfill your greatest groove-related dreams. Each issue features
-    reviews on the latest drums, percussion, and hardware available. In addition, we will
-    include interviews and tips, as well as tablature from some of the most world-renowned
-    drummers! Our magazine appeals to drummers of all genres and skill levels. With a little
-    something for everyone, you are guaranteed to enjoy Drum Lover Magazine and you will even
-    improve as a musician along the way! We will even give you the first issue free of charge!
-    So what are you waiting for? <a href=form.php>SUBSCRIBE NOW!</a></p>
-        
+    <h3>> Cold Era Drummer on Cover of Drum Lover's Magazine</h3>
+    <p>
+        <img class='h3img' src='austindrums.jpg' width='30%' height="30%" alt='Austin drumming at live show'>
+        Austin Batistoni has been featured on Drum Lover's Magazine!  Find him on the cover of this infamous magazine
+        that's also featured world renowned drummers including Dave Grohl, Keith Moon, Travis Barker, and John Bonham.  
+        Flip to page 14 to read his full interview.
+    </p>
+    
+
+   
+    
     <?php
     include ("footer.php");
     ?>
--- music.php
+++ music.php
@@ -1,8 +1,32 @@
 <?php
+    include ("top.php");
+    include ("header.php");
+    include ("nav.php");
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<h1 id='music_header'>New Music</h1>
 
+<p class='music'>Stay tuned for these new tracks off of our upcoming album, being released in July 2016.</p>
+<ul id='music_list'><br>
+    <br>
+    <li>Culprit</li><br>
+    <li>Sandbox</li><br>
+    <li>Shit</li><br>
+    <li>Wires for Breakfast</li>
+</ul>
+
+    <img id='music_img' src='pics/coldera.jpg' alt=''>
+
+<p id='music'>An exclusive preview of our new release, Sandbox:</p>
+<audio controls="play pause volume">
+    <source src="Sandbox.mp3" type="audio/ogg">
+    <source src="Sandbox.mp3" type="audio/mpeg">
+    Your browser does not support this audio tag.
+</audio>
+
+<p>Sign up for our <a href='form.php'>mailing list</a> to get more samples sent to you before they're released!</p>
+
+
+<?php
+include ("footer.php");
+?>
